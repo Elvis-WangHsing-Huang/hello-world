fa5f16f6c429f6c756941eba4cbcc70988dd8019
Elvis-WangHsing-Huang <elviswshuang@gmail.com>
2017-05-21 19:31:53 +0800

    class prototyping for ex43

--- python-test/ex43.py
+++ python-test/ex43.py
@@ -1,3 +1,52 @@
+
+class Engine(object):
+    def __init__(self, map):
+        pass
+    def play(self):
+        pass
+
+class Map(object):
+
+    # Give a map of all scenes => in dictionary {name:object, ...}
+    # How to current scene  will enter the next scene
+    # => is decided in each scene, scenarios-based
+
+    def __init__(self, start_scene):
+        pass
+    def opening_scene(self):
+        pass
+    def next_scene(self, scene_name):
+        pass
+
 class Scene(object):
     def enter(self):
         pass
+
+
+class Death(Scene):
+    def enter(self): #this will override parent memeber function
+        pass
+
+
+class CentralCorridor(Scene):
+    def enter(self):
+        pass
+
+class LaserWeaponArmory(Scene):
+    def enter(self):
+        pass
+
+class TheBridge(Scene):
+    def enter(self):
+        pass
+
+
+class EscapePod(Scene):
+    def enter(self):
+        pass
+
+
+
+a_map = Map("central_corridor")
+a_game = Engine(a_map)
+a_game.play()
