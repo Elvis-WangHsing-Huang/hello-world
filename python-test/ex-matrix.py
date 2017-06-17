7becde4818b38dc0e6e78858186c41a8c54e8671
Elvis-WangHsing-Huang <elviswshuang@gmail.com>
2017-06-17 11:25:11 +0800

    solve linear equation using iteration method

--- python-test/ex-matrix.py
+++ python-test/ex-matrix.py
@@ -17,3 +17,19 @@ b = np.array([20., 33., 36.])
 x = invA.dot(b) # invA.dot(b) => b is treated as a vector (3X1)
                 # not invA*b => b is treaded as a matrix (1X3)
 print(x)
+
+
+# the other way to calculate x by iteration
+
+# X1 =    0*X1+ 3/8*X2 - 2/8*X3 + 20/8
+# X2 =-4/11*X1+   0*X2 +1/11*X3 + 33/11
+# X3 =-6/12*X1-3/12*X2 +   0*X3 + 36/12
+#
+# X = B*X + f
+
+B = np.array([[0.,3./8., -2./8. ], [-4./11., 0., 1./11.], [-6./12., -3./12., 0]])
+f = np.array([20./8., 33./11., 36./12.])
+X = np.array([0., 0., 0.]) # initiallize a zero vector
+for i in range(10):
+    X = B.dot(X)+f
+    print("iteration",i, X)
